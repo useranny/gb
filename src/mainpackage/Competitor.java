f3e8786523eccda0054b9964b81c2b663f628c39
anny <85-a@mail.ru>
2019-10-01 20:55:27 +0300

    hw5

--- src/mainpackage/Competitor.java
+++
@@ -1,9 +0,0 @@
-package mainpackage;
-
-public interface Competitor {
-    void run(int dist);
-    void swim(int dist);
-    void jump(int height);
-    boolean isOnDistance();
-    void info();
-}
--- src/participants/Cat.java
+++
@@ -1,7 +0,0 @@
-package participants;
-
-public class Cat extends mainpackage.Animal {
-    public Cat(String name) {
-        super("Кот", name, 200, 20, 0);
-    }
-}
