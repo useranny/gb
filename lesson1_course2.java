785c0de9e9b2fa00c00a7131bb7e5dc4d5c5f945
anny <85-a@mail.ru>
2019-10-19 17:06:35 +0300

    1 and 2 task

---
+++ lesson1_course2.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class lesson1_course2 {
+    public static void main(String[] args) {
+
+
+
+    String[] ArrStr = {"Иван","Андрей","Алексей","Борис","Владимир","Геннадий","Николай","Пётр","Родион","Сергей"};
+
+//        ____________1_____________
+//        System.out.println(Arrays.toString(ArrStr));
+//        String help;
+//        help = ArrStr[0];
+//        ArrStr[0] = ArrStr[5];
+//        ArrStr[5] = help;
+//        help = null;
+//        System.out.println(Arrays.toString(ArrStr));
+
+
+
+
+//        ______________2_____________
+//
+//        List<String> list = Arrays.asList(ArrStr);
+//        System.out.println(list);
+
+    }
+}
